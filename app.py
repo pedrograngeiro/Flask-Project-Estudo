f83968bca63c988537ffb9128344aeb6f42e40c7
Pedro Grangeiro <pedrop.grangeiro@gmail.com>
2023-12-21 15:00:13 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def principal():
+    return render_template("index.html")
+
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
